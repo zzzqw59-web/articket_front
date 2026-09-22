286b03061ecb20770591891d2a605468be162b27
Mute <https://github.com/zzzqw59-web>
2026-09-22 12:11:46 +0900

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import { RouterProvider } from 'react-router-dom'
+
+function App() {
+
+  return <RouterProvider router={root} />
+}
+
+export default App
---
+++ src/router/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+
+const roots = createBrowserRouter([
+  // 메인페이지
+  {
+    path: "/",
+    HydrateFallback: () => <div>Loading...</div>,
+    lazy: async () => {
+      const { default: Component } = await import("../pages/MainPage");
+      return { Component };
+    },
+  },
+]);
+
+export default roots
